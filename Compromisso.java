a158a6e738beb0c847fde29093b307cbfed4e6a8
Lucas Andrade Rodrigues <lucas.andrade.rodrigues.13@gmail.com>
2018-10-17 00:15:15 -0300

    Update Compromisso.java

--- Compromisso.java
+++ Compromisso.java
@@ -2,6 +2,7 @@ package correctroutine;
 import java.util.Calendar;
 import java.util.Map;
 import java.util.HashMap;
+import javax.swing.JOptionPane;
 
 public class Compromisso {
     String acronimo;
@@ -9,15 +10,17 @@ public class Compromisso {
     String professor;
     
     Calendar calendario = Calendar.getInstance();
-    int dia = calendario.get(Calendar.DAY_OF_MONTH);
-    int mes = calendario.get(Calendar.MONTH);
-    int ano = calendario.get(Calendar.YEAR);
-    int hora = calendario.get(Calendar.HOUR_OF_DAY);
-    int minuto = calendario.get(Calendar.MINUTE);
+    int diaHj = calendario.get(Calendar.DAY_OF_MONTH);
+    int mesHj = calendario.get(Calendar.MONTH);
+    int anoHj = calendario.get(Calendar.YEAR);
+    int horaHj = calendario.get(Calendar.HOUR_OF_DAY);
+    int minutoHj = calendario.get(Calendar.MINUTE);
+    //  dia/mes/ano hora:minuto
+    String dataComp = diaHj+"/"+mesHj+"/"+anoHj+" "+horaHj+":"+minutoHj;
     String tag;
     Map<String, String> compromissos = new HashMap<>();
     Map<String, String> horarios = new HashMap<>();
-    
+     
   public void adcComprmisso(String acronimo, String disciplina, String professor, String data){
   compromissos.put(acronimo,disciplina+" "+professor);
   horarios.put(acronimo, data);
@@ -35,8 +38,13 @@ public class Compromisso {
   public void excluirComp(String acronimo){
   compromissos.remove(acronimo);
   }
-  public void EditComp(String acronimo){
-  
+  public void concluirComp(){
+  for(String acronimo:horarios.keySet()){
+      if(horarios.get(acronimo).equals(dataComp)){
+          JOptionPane.showMessageDialog(null, acronimo+" Terminado");
+          excluirComp(acronimo);
+      }
+  }
   }
   
 }
