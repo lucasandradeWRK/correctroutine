254410ffe1979236c4042a85defef037ee93cc1d
Lucas Andrade Rodrigues <lucas.andrade.rodrigues.13@gmail.com>
2018-10-17 00:16:09 -0300

    Update CorrectRoutine.java

--- CorrectRoutine.java
+++ CorrectRoutine.java
@@ -1,12 +1,24 @@
 package correctroutine;
 
+import java.util.Calendar;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class CorrectRoutine {
     public static void main(String[] args) {
+        Calendar calendario = Calendar.getInstance();
+        int dia = calendario.get(Calendar.DAY_OF_MONTH);
+        int mes = calendario.get(Calendar.MONTH);
+        int ano = calendario.get(Calendar.YEAR);
+        int hora = calendario.get(Calendar.HOUR_OF_DAY);
+        int minuto = calendario.get(Calendar.MINUTE);
+       
        Compromisso comp = new Compromisso();
-       comp.adcComprmisso("redacao Pt","Portugues", "Gerson", "10-09-2018 10:30:00");
-       comp.adcComprmisso("atv Mt","Matematica", "Lobaoo", "13-09-2018 04:15:00");
-       comp.adcComprmisso("prov gf","geografia", "thiago", "05-07-2018 09:30:00");
+       comp.adcComprmisso("redacao Pt","Portugues", "Gerson", "10/09/2018 10:30");
+       comp.adcComprmisso("atv Mt","Matematica", "Lobaoo", "13/09/2018 04:15");
+       comp.adcComprmisso("prov gf","geografia", "thiago", "17/9/2018 0:7");
        comp.excluirComp("redacao Pt");
+       comp.concluirComp();
        comp.lista();
     }
     
